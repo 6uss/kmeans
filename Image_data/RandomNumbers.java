273e4eb761482993b575705321986b068d54cd6f
Lcast162 <32620504+Lcast162@users.noreply.github.com>
2017-12-04 17:54:27 -0500

    Add files via upload

---
+++ Image_data/RandomNumbers.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package randomnumbers;
+
+import java.util.Random;
+
+/**
+ *
+ * @author vh133
+ */
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+public class RandomNumbers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try{
+            
+        
+        double temp =0;
+        File file = new File("test10Million.txt");
+        PrintWriter writeTo = new PrintWriter(file);
+        for(int i=0; i<10000000; i++)
+        {
+            writeTo.write(i + " ");
+            for(int j = 0; j < 9; j++)
+            {
+                Random gen= new Random();
+                temp = gen.nextDouble()+gen.nextInt(10);
+                String tempString = "";
+                tempString += temp;
+                tempString = String.format("%1.8s", tempString);
+                tempString += " ";
+                writeTo.write(tempString);
+                
+            }
+            writeTo.write("\n");
+        }
+        writeTo.close();
+        }
+        catch(FileNotFoundException ex)
+        {
+            System.out.println("Ya dungoofed");
+        }
+        
+    }
+    
+}
